8bc46ae3a73e31aa3d4ee5f44ef7eb8dc7a73002
Purinat Sanbundit <37452245+PurinatG-01@users.noreply.github.com>
2023-05-11 23:16:47 +0700

    init: init repo

---
+++ src/app/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image"
+import Card from "@/components/main/Card"
+import { useEffect, useState } from "react"
+import axios from "axios"
+export default function Home() {
+  const [response, setResponse] = useState()
+
+  useEffect(() => {
+    ;(async function () {
+      try {
+        const response = await axios.get<{msg: string, status: number}>(
+          "https://gin-main-swbyfznsjq-de.a.run.app/test"
+        )
+        setResponse(response)
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+  }, [])
+  return (
+    <main className="flex min-h-screen flex-col gap-4 p-24">
+      <h1 className="text-3xl tracking-widest text-neutral-200 font-bold">
+        POPPER
+      </h1>
+      <Card>
+        <article className="flex flex-col gap-2">
+          <h2 className="text-xl font-bold text-neutral-200 text-">
+            Testing NEXT.JS 13
+          </h2>
+          <p className="font-light text-neutral-300 tex-xs indent-4">
+            Veniam tempor elit consequat in est cillum aliqua sunt nisi enim
+            amet veniam. Amet enim nisi laborum nulla culpa commodo. Laborum
+            excepteur cupidatat sunt ipsum laboris elit est laboris ullamco.
+            Proident ullamco in ad minim anim. Dolore id labore consequat
+            consequat. Tempor sint fugiat adipisicing dolor.
+          </p>
+        </article>
+      </Card>
+    </main>
+  )
+}
---
+++ src/components/main/Card.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Card(props) {
+  const { children } = props
+  return (
+    <div
+      className="p-4 border border-neutral-800
+    min-w-[100px] min-h-[100px] bg-neutral-950 rounded-lg hover:-translate-y-4
+    transition-all
+  "
+    >
+      {children}
+    </div>
+  )
+}
