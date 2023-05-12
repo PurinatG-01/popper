1a8efd6601fad0380dbbd26d0bd76f55426f42a3
Purinat Sanbundit <37452245+PurinatG-01@users.noreply.github.com>
2023-05-12 10:46:10 +0700

    feat: update type

--- src/components/main/Card.tsx
+++ src/components/main/Card.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 
-export default function Card(props) {
-  const { children } = props
+export default function Card({ children }: { children: React.ReactNode }) {
   return (
     <div
       className="p-4 border border-neutral-800
